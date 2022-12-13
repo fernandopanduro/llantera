fff225ae1b16f61bfe612d29021cbf0d96d07f9a
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-12-13 12:04:10 -0700

    Agregado efecto Fade

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { Fade } from 'react-reveal';
 
 import {BsTelephoneOutboundFill} from 'react-icons/bs'
 import {AiFillHome} from 'react-icons/ai';
@@ -14,29 +15,30 @@ export function Header() {
     const [ isOpen, setIsOpen] = useState(false)
 
     return (
-        <section className={`${styles.header}`}>        
-            <div className={styles.headerContainer}>
-                <Link to='/'><h1 className={styles.title}>Llantera Mazatlan</h1></Link>
-                <ul className={styles.list}>
-                    <li className={styles.listItem}><a href='#'><AiFillHome size={23} className={styles.icon} /> Inicio</a></li>
-                    <li className={styles.listItem}><a href='#'> <MdHomeRepairService size={23} className={styles.icon} /> Servicios</a></li>
-                    <li className={styles.listItem}><a href='#'><GrMail size={23} className={styles.icon} />Contacto</a></li>
-                </ul>
-                <p className={styles.phoneNumber}><BsTelephoneOutboundFill className={styles.phoneIcon} /> 669 123 5660</p>
-                <ul className={`${styles.listResponsive} ${isOpen && styles.activelistResponsive}`}>
-                    <li className={styles.listItem}><a href='#'><AiFillHome size={23} className={styles.icon} /> Inicio</a></li>
-                    <li className={styles.listItem}><a href='#'> <MdHomeRepairService size={23} className={styles.icon} /> Servicios</a></li>
-                    <li className={styles.listItem}><a href='#'><GrMail size={23} className={styles.icon} />Contacto</a></li>
-                    <li className={`${styles.phoneNumber} ${styles.listItem}`}><BsTelephoneOutboundFill className={styles.phoneIcon} /> 669 123 5660</li>
-                </ul>
+        <section className={`${styles.header}`}> 
+            <Fade top>
+                <div className={styles.headerContainer}>
+                    <Link to='/'><h1 className={styles.title}>Llantera Mazatlan</h1></Link>
+                    <ul className={styles.list}>
+                        <li className={styles.listItem}><a href='#'><AiFillHome size={23} className={styles.icon} /> Inicio</a></li>
+                        <li className={styles.listItem}><a href='#'> <MdHomeRepairService size={23} className={styles.icon} /> Servicios</a></li>
+                        <li className={styles.listItem}><a href='#'><GrMail size={23} className={styles.icon} />Contacto</a></li>
+                    </ul>
+                    <p className={styles.phoneNumber}><BsTelephoneOutboundFill className={styles.phoneIcon} /> 669 123 5660</p>
+                    <ul className={`${styles.listResponsive} ${isOpen && styles.activelistResponsive}`}>
+                        <li className={styles.listItem}><a href='#'><AiFillHome size={23} className={styles.icon} /> Inicio</a></li>
+                        <li className={styles.listItem}><a href='#'> <MdHomeRepairService size={23} className={styles.icon} /> Servicios</a></li>
+                        <li className={styles.listItem}><a href='#'><GrMail size={23} className={styles.icon} />Contacto</a></li>
+                        <li className={`${styles.phoneNumber} ${styles.listItem}`}><BsTelephoneOutboundFill className={styles.phoneIcon} /> 669 123 5660</li>
+                    </ul>
 
-                <div className={styles.barsMenu} onClick={() =>  setIsOpen(!isOpen)}>
-                    <span className={`${styles.line1__barsMenu} ${isOpen && styles.activeline1__barsMenu}`}></span>
-                    <span className={`${styles.line2__barsMenu} ${isOpen && styles.activeline2__barsMenu}`}></span>
-                    <span className={`${styles.line3__barsMenu} ${isOpen && styles.activeline3__barsMenu}`}></span>
+                    <div className={styles.barsMenu} onClick={() =>  setIsOpen(!isOpen)}>
+                        <span className={`${styles.line1__barsMenu} ${isOpen && styles.activeline1__barsMenu}`}></span>
+                        <span className={`${styles.line2__barsMenu} ${isOpen && styles.activeline2__barsMenu}`}></span>
+                        <span className={`${styles.line3__barsMenu} ${isOpen && styles.activeline3__barsMenu}`}></span>
+                    </div>
                 </div>
-
-            </div>
+            </Fade>       
         </section>
     )
 }
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -8,47 +8,58 @@ import {HiArrowLongDown} from 'react-icons/hi2';
 import {BsTelephoneOutboundFill} from 'react-icons/bs'
 import {IoLogoWhatsapp} from 'react-icons/io';
 import {FaRegCalendarAlt} from 'react-icons/fa';
+import { Fade } from 'react-reveal';
  
 export function LandingPage() {
     return (
         <section className={styles.landingPage}>
 
             <div className={styles.banner} id='home'>
-                <div className={styles.imgContainer}>
-                    <img src={imgBanner} className={styles.img} alt="" />
-                </div>
+                <Fade>
+                    <div className={styles.imgContainer}>
+                        <img src={imgBanner} className={styles.img} alt="" />
+                    </div>
+                </Fade>
                 <div className={styles.presentation}>
-                    <div className={styles.presentationText}>
-                        <h2 className={styles.title}>Llantera Mazatlan</h2> 
-                        <a href="#contact">
-                            <button className={styles.button}>
-                                <BsTelephoneOutboundFill className={styles.phone} size={23} />
-                                 Llama Ahora
-                            </button>
-                        </a>
-                    </div>
-                    <div className={styles.scroll}>
-                        <BsFillMouseFill size={23} />
-                        <HiArrowLongDown className={styles.arrow} size={23} />
-                    </div>
+                    <Fade bottom>
+                        <div className={styles.presentationText}>
+                            <h2 className={styles.title}>Llantera Mazatlan</h2> 
+                            <a href="#contact">
+                                <button className={styles.button}>
+                                    <BsTelephoneOutboundFill className={styles.phone} size={23} />
+                                    Llama Ahora
+                                </button>
+                            </a>
+                        </div>
+                        <div className={styles.scroll}>
+                            <BsFillMouseFill size={23} />
+                            <HiArrowLongDown className={styles.arrow} size={23} />
+                        </div>
+                    </Fade>
                 </div>
             </div>
 
-            <ul className={styles.bottomList}>
-                <li className={styles.bottomListItem}><BsTelephoneOutboundFill size={23} /></li>
-                <li className={styles.bottomListItem}><IoLogoWhatsapp size={23} /></li>
-                <li className={styles.bottomListItem}><FaRegCalendarAlt size={23} /></li>
-            </ul>
+            <Fade right>
+                <ul className={styles.bottomList}>
+                    <li className={styles.bottomListItem}><BsTelephoneOutboundFill size={23} /></li>
+                    <li className={styles.bottomListItem}><IoLogoWhatsapp size={23} /></li>
+                    <li className={styles.bottomListItem}><FaRegCalendarAlt size={23} /></li>
+                </ul>
+            </Fade>
 
             <section className={`${styles.about} ${styles.sectionContainer} container`}>
-                <div className={styles.aboutContainerImg}>
-                    <img src={imgAbout} className={`${styles.img} ${styles.imgAbout}`} alt="" />
-                </div>
+                <Fade left>
+                    <div className={styles.aboutContainerImg}>
+                        <img src={imgAbout} className={`${styles.img} ${styles.imgAbout}`} alt="" />
+                    </div>
+                </Fade>
                 <div className={styles.aboutDescription}>
-                    <h3 className={styles.subtitle}>Llantera Garcia en Mazatlan Sinaloa contamos con tecnicos de Calidad</h3>
-                    <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
-                    <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
-                    <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
+                    <Fade bottom>
+                        <h3 className={styles.subtitle}>Llantera Garcia en Mazatlan Sinaloa contamos con tecnicos de Calidad</h3>
+                        <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
+                        <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
+                        <p className={styles.paragrath}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae sunt impedit, adipisci tempora deserunt maxime, aliquid temporibus amet fugit, eaque numquam. Tenetur reprehenderit aliquid inventore molestiae unde, mollitia velit quasi?</p>
+                    </Fade>
                 </div>
             </section>
 
