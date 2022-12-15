13cec129d102e02d8c9051efbfce7127b9c9f1fb
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-12-14 19:22:31 -0700

    Mejorado el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,25 +5,51 @@ import discover from '../assets/images/discover.png';
 import american from '../assets/images/american-express.png';
 
 import Fade from 'react-reveal/Fade';
+import { Link } from 'react-router-dom';
+
+import {AiFillFacebook, AiFillInstagram} from 'react-icons/ai';
+import {FaTiktok} from 'react-icons/fa';
 
 export function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={`${styles.footerContainer} container`}>
                 <Fade bottom>
-                    <ul className={styles.list}>
-                        <li className={`${styles.listItem} ${styles.listItemMenu}`}><a href='#services'>Servicios</a></li>
-                        <li className={`${styles.listItem} ${styles.listItemMenu}`}><a href='#home'>Inicio</a></li>
-                        <li className={`${styles.listItem} ${styles.listItemMenu}`}><a href='#contact'>Contacto</a></li>
-                    </ul>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><img src={visa} alt="" /></li>
-                        <li className={styles.listItem}><img src={discover} alt="" /></li>
-                        <li className={styles.listItem}><img src={american} alt="" /></li>
-                    </ul>
-                    <p>Llantera Garcia</p>
+                    <div className={styles.footerList}>
+                        <p className={styles.footerSubmenu}>Llantera Garcia</p>
+                        <ul className={`${styles.footerList} ${styles.icons}`}>
+                            <li><a href='facebook'><AiFillFacebook size={22}/></a></li>
+                            <li><a href='instagram'><AiFillInstagram size={22}/></a></li>
+                            <li><a href='tiktok'><FaTiktok size={22}/></a></li>
+                        </ul>
+                    </div>
+                    <div className={styles.footerList}>
+                        <p className={styles.footerSubmenu}>Menu</p>
+                        <ul className={styles.footerList}>
+                            <Fade bottom>
+                                <li className={styles.footerListItem}><a href='#home'>Inicio</a></li>
+                                <li className={styles.footerListItem}><a href='#services'>Servicios</a></li>
+                                <li className={styles.footerListItem}><a href='#contact'>Contacto</a></li>
+                            </Fade>
+                        </ul>
+                    </div>
+                    <div className={styles.footerList}>
+                        <p className={styles.footerSubmenu}>Acerca De</p>
+                        <ul className={styles.footerList}>
+                            <Fade bottom>
+                                <li className={styles.footerListItem}><Link /* to='/privacy' */>Privacidad</Link> </li>
+                                <li className={styles.footerListItem}><Link /* to='/terms' */>Terminos</Link> </li>
+                                <li className={styles.footerListItem}><Link /* to='/#contact' */>Contacto</Link> </li>
+                                <li className={styles.footerListItem}><Link /* to='/refund' */>Devoluciones</Link> </li>
+                                <li className={styles.footerListItem}><Link /* to='/shipping' */>Compra</Link> </li>
+                            </Fade>
+                        </ul>
+                    </div> 
                 </Fade>
             </div>
+            <Fade bottom>
+                <p className={styles.copy}>&copy; Llantera Garcia</p> 
+            </Fade>
         </footer>
     )
 }
